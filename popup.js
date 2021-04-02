97cae3286c78bfd9c573c6a687d39c56678c8241
tery-gasmen <teryrambin@gmail.com>
2021-04-02 16:02:10 +0800

    collapsible sidebar working

--- popup.js
+++ popup.js
@@ -1,14 +1,13 @@
-var coll = document.getElementsByClassName("sidebar_cont");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+/* Set the width of the sidebar to 28vw and the left margin of the page content to 28vw */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "30vw";
+  document.getElementById("main").style.marginLeft = "30vw";
 }
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
