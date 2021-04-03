ce19e27a5372289a988b0e75bd7765fb59363bc1
tery-gasmen <teryrambin@gmail.com>
2021-04-03 17:50:45 +0800

    darkmode working

--- popup.js
+++ popup.js
@@ -20,8 +20,26 @@ function fullscreen() {
 function darkmode() {
   if( $( "body" ).hasClass( "darkmode__enabled" )) {
     $( "body" ).removeClass( "darkmode__enabled" );
+    $( ".sidebar_cont" ).removeClass( "darkmode__enabled__two" );
+    $( ".closebtn" ).removeClass( "darkmode__enabled__two" );
+    $( ".title_cont" ).removeClass( "darkmode__enabled__two" );
+    $( ".decks_cont" ).removeClass( "darkmode__enabled__two" );
+    $( ".main_deck" ).removeClass( "darkmode__enabled__two" );
+    $( ".subdeck" ).removeClass( "darkmode__enabled__two" );
+    $( ".list" ).removeClass( "darkmode__enabled__two" );
+    $( ".title_input" ).removeClass( "darkmode__enabled__two" );
+
+  
   } else {
     $( "body" ).addClass( "darkmode__enabled" );
+    $( ".sidebar_cont" ).addClass( "darkmode__enabled__two" );
+    $( ".closebtn" ).addClass( "darkmode__enabled__two" );
+    $( ".title_cont" ).addClass( "darkmode__enabled__two" );
+    $( ".decks_cont" ).addClass( "darkmode__enabled__two" );
+    $( ".main_deck" ).addClass( "darkmode__enabled__two" );
+    $( ".subdeck" ).addClass( "darkmode__enabled__two" );
+    $( ".list" ).addClass( "darkmode__enabled__two" );
+    $( ".title_input" ).addClass( "darkmode__enabled__two" );
   }
 }
 
