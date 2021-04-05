8d9f6d42beedb760fdad208945f3e76eebc18378
tery-gasmen <teryrambin@gmail.com>
2021-04-05 18:10:32 +0800

    disappearing text working properly

--- textfield.js
+++ textfield.js
@@ -1,11 +1,12 @@
 var searchTimeout;
 document.getElementById('user_txt').onkeypress = function () {
-    if (searchTimeout != undefined) {
-        clearTimeout(searchTimeout);
-    } else {
-        searchTimeout = setTimeout(callServerScript, 5000);
-    }
+    if (searchTimeout != undefined) clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(callServerScript, 10000);
 };
+
 function callServerScript() {
-    document.getElementById('user_txt').innerText = '';
+    $('#user_txt').fadeOut(1000, function() {
+        $('#user_txt').val('');
+    });
+    $('#user_txt').fadeIn(0);
 }
