751cab1cac04735b5b0860c4b6b2dd98bd9a7225
tery-gasmen <teryrambin@gmail.com>
2021-04-10 12:32:49 +0800

    sidebar on mobile/tablet version working

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 28vw and the left margin of the page content to 25vw */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "25vw";
-  document.getElementById("main").style.marginLeft = "25vw";
+  document.getElementById("mySidebar").style.width = "380px";
+  document.getElementById("main").style.marginLeft = "380px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
